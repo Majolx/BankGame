99104cb938b4fa93392eb8410e68a836848c0626
majolx <vts815@gmail.com>
2013-10-22 11:01:17 -0700

    More Formatting

--- BankGame/BankGame/Character/BaseCharacter.cs
+++ BankGame/BankGame/Character/BaseCharacter.cs
@@ -1,18 +1,71 @@
+#region Using Statements
 using System.Collections.Generic;
 using System;					// Added to access the enum class
+#endregion
 
-public class BaseCharacter {
-	private string name;
+/// <summary>
+/// The BaseCharacter class provides fields and methods for
+/// every type of character entity in the game, both player
+/// and computer controlled.
+/// </summary>
+/// <remarks>
+/// This class should provide a base for each character entity.
+/// Because of this, only place fields and methods here which
+/// would be applicable for every character.
+/// </remarks>
+public class BaseCharacter
+{
+
+    #region Fields
+
+    private string name;
 	private int level;
-	private uint freeExp;
+	private uint freeExp; // We use uint instead of int because we won't
+                          // be dealing with negative experience values.
 	
 	private Attribute[] primaryAttribute;
 	private Vital[] vital;
 	private Skill[] skill;
-	
-	//private List<Buff> buffs;
-	
-	public void Awake() {
+
+    //private List<Buff> buffs;
+    //private List<Consumable> inventory;
+    //private List<WearableItem> equipment;
+
+    #endregion
+
+    #region Properties
+
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public uint FreeExp
+    {
+        get { return freeExp; }
+        set { freeExp = value; }
+    }
+
+    public void addExp(uint exp)
+    {
+        freeExp += exp;
+        calculateLevel();
+    }
+
+
+    #endregion
+    public void Awake() {
 		name = string.Empty;
 		level = 0;
 		freeExp = 0;
@@ -27,29 +80,11 @@ public class BaseCharacter {
 	
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// 
+	/// </summary>
 	void Update () {
 	
-	}
-	
-	public string Name {
-		get{ return name; }
-		set{ name = value; }
-	}
-	
-	public int Level {
-		get{ return level; }
-		set{ level = value; }
-	}
-	
-	public uint FreeExp {
-		get{ return freeExp; }
-		set{ freeExp = value; }
-	}
-	
-	public void addExp(uint exp) {
-		freeExp += exp;
-		calculateLevel();
 	}
 	
 	// Take the average of all of the player's skills and assign it as the player level.
