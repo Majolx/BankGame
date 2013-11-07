01e3e7d4502f41dd7091fd3f88ec18f7fc9c1be1
majolx <vts815@gmail.com>
2013-11-07 10:29:06 -0800

    Character Test Screen

---
+++ BankGame/BankGame/Screens/Debug/CharacterTestScreen.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BankGame.Screens
+{
+    class CharacterTestScreen : GameScreen
+    {
+        private BaseCharacter testCharacter;
+        private string output;
+
+        public CharacterTestScreen()
+        {
+            testCharacter = new BaseCharacter();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+            output = "Name: " + testCharacter.Name +
+                  "\nLevel: " + testCharacter.Level;
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(ScreenManager.Font, output, new Vector2(0, 0), Color.White);
+            spriteBatch.End();
+        }
+    }
+}
