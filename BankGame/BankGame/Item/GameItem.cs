7784ab5cdf2f8bd81c872484552c8079484c0944
holofernes <pinghanmonkey@gmail.com>
2013-12-12 03:02:06 -0800

    Updates to item manager and base game item

---
+++ BankGame/BankGame/Item/GameItem.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Game1.ItemClasses
+{
+ public class GameItem
+ {
+ #region Field Region
+ public Vector2 Position;
+ private Texture2D image;
+ private Rectangle? sourceRectangle;
+ private readonly BaseItem baseItem;
+ private Type type;
+ #endregion
+ #region Property Region
+public Texture2D Image
+ {
+ get { return image; }
+ }
+ public BaseItem Item
+ {
+ get { return baseItem; }
+ }
+ public Type Type
+ {
+ get { return type; }
+ }
+ #endregion
+ #region Constructor Region
+ public GameItem(BaseItem item, Texture2D texture)
+ {
+ baseItem = item;
+ image = texture;
+ type = item.GetType();
+ }
+ #endregion
+ #region Method Region
+ public void Draw(SpriteBatch spriteBatch)
+ {
+     //draw logic
+ }
+ #endregion
+
--- BankGame/BankGame/Item/ItemManager.cs
+++ BankGame/BankGame/Item/ItemManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 namespace Game1.ItemClasses
 {
  public class ItemManager
